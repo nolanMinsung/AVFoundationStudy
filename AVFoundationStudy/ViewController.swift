9f32e76a7771611eded2de1ca664c42b0be208df
Minsung Kim <mskim4048@naver.com>
2025-05-25 20:48:02 +0900

    Initial Commit

---
+++ AVFoundationStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVFoundationStudy
+//
+//  Created by 김민성 on 5/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
